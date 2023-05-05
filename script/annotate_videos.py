fffb596cc4424467cfa4684f686492b68f4834fc
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-05 12:14:08 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -1,21 +1,22 @@
-import json
 import os
+import json
 
 annotation_file = "/iscc/git/FIVR-200K/dataset/annotation.json"
 src_dir = "/iscc/videos720/"
 dest_dir = "/iscc/annotated_videos/"
 
-with open(annotation_file, 'r') as f:
+# Load annotation data from JSON file
+with open(annotation_file, "r") as f:
     data = json.load(f)
 
-for video in data:
-    video_id = video["video_id"]
-    for segment in video["segments"]:
-        start_time = segment["start_time"]
-        end_time = segment["end_time"]
-        video_file = f"{video_id}.mp4"
-        src_path = os.path.join(src_dir, video_file)
-        dest_path = os.path.join(dest_dir, video_id, video_file)
-        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-        os.rename(src_path, dest_path)
-        print(f"Annotated video {src_path} moved to {dest_path}")
+# Loop through videos in the annotation data
+for video_id, video_data in data.items():
+    video_file = video_data["file_path"].split("/")[-1]
+    src_path = os.path.join(src_dir, video_file)
+    dest_path = os.path.join(dest_dir, video_data["query"], video_file)
+    
+    # Create directory if it doesn't exist
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    
+    # Move video file to destination directory
+    os.rename(src_path, dest_path)
