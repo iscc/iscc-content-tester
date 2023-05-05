626b92ff6dc08b23171cb73f61bc7f407559b115
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-05 12:13:39 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -5,15 +5,17 @@ annotation_file = "/iscc/git/FIVR-200K/dataset/annotation.json"
 src_dir = "/iscc/videos720/"
 dest_dir = "/iscc/annotated_videos/"
 
-with open(annotation_file, "r") as f:
+with open(annotation_file, 'r') as f:
     data = json.load(f)
 
-for video_id, video_data in data.items():
-    video_file = video_id + ".mp4"
-    src_path = os.path.join(src_dir, video_file)
-    dest_path = os.path.join(dest_dir, video_data["query"], video_file)
-    if os.path.exists(src_path):
+for video in data:
+    video_id = video["video_id"]
+    for segment in video["segments"]:
+        start_time = segment["start_time"]
+        end_time = segment["end_time"]
+        video_file = f"{video_id}.mp4"
+        src_path = os.path.join(src_dir, video_file)
+        dest_path = os.path.join(dest_dir, video_id, video_file)
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
         os.rename(src_path, dest_path)
-    else:
-        print(f"Video {src_path} not found.")
+        print(f"Annotated video {src_path} moved to {dest_path}")
