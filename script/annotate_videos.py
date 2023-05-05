48583e7e98455255d06961be9895455645d1b76a
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-05 12:12:41 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -8,16 +8,12 @@ dest_dir = "/iscc/annotated_videos/"
 with open(annotation_file, "r") as f:
     data = json.load(f)
 
-for video in data["videos"]:
-    video_name = video["video_id"] + ".mp4"
-    if os.path.isfile(os.path.join(src_dir, video_name)):
-        for annotation in video["annotations"]:
-            start_time = annotation["segment"][0]
-            end_time = annotation["segment"][1]
-            dest_path = os.path.join(dest_dir, annotation["query"], video_name)
-            os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-            src_path = os.path.join(src_dir, video_name)
-            os.system(f"ffmpeg -i {src_path} -ss {start_time} -to {end_time} -c:v libx264 -c:a aac -strict experimental -b:a 192k -ac 2 -ar 44100 -vf scale=640:360 {dest_path}")
-            print(f"Video '{video_name}' annotated with query '{annotation['query']}' and saved to '{dest_path}'")
+for video_id, video_data in data.items():
+    video_file = video_id + ".mp4"
+    src_path = os.path.join(src_dir, video_file)
+    dest_path = os.path.join(dest_dir, video_data["query"], video_file)
+    if os.path.exists(src_path):
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+        os.rename(src_path, dest_path)
     else:
-        print(f"Video '{video_name}' not found in '{src_dir}'")
+        print(f"Video {src_path} not found.")
