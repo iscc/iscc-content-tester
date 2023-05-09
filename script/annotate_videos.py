97c11477f236eed02352c14bac336a02674096cf
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-09 13:15:23 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -10,16 +10,19 @@ src_dir = '/iscc/videos720/'
 dest_dir = '/iscc/annotated_videos/'
 
 total_videos = sum(len(data[key][sub_key]) for key in data for sub_key in data[key])
-pbar = tqdm(total=total_videos, desc="Copying videos")
+pbar = tqdm(total=total_videos, desc="Copying videos", unit=" video")
 
 for key in data:
-    if not os.path.isfile(os.path.join(src_dir, key + '.mp4')):
+    print(f"Processing key: {key}")
+    if key + '.mp4' not in os.listdir(src_dir):
+        print(f"{key}.mp4 not found in {src_dir}. Skipping...")
         continue
     
     dest_key_dir = os.path.join(dest_dir, key)
     os.makedirs(dest_key_dir, exist_ok=True)
     
     for sub_key in data[key]:
+        print(f"Processing subkey: {sub_key}")
         dest_sub_dir = os.path.join(dest_key_dir, sub_key)
         os.makedirs(dest_sub_dir, exist_ok=True)
         
@@ -28,11 +31,14 @@ for key in data:
             dest_path = os.path.join(dest_sub_dir, video_id + '.mp4')
             
             if not os.path.isfile(src_path):
+                print(f"{src_path} not found. Skipping...")
                 pbar.update(1)
-                print(f"Video '{src_path}' not found.")
                 continue
             
+            print(f"Copying {src_path} to {dest_path}")
             shutil.copyfile(src_path, dest_path)
             pbar.update(1)
             remaining_videos = total_videos - pbar.n
-            pbar.set_postfix(eta=f"{remaining_videos // pbar.average:.0f}s")
+            pbar.set_postfix(completed=f"{pbar.n}/{total_videos}", eta=f"{remaining_videos // pbar.rate:.0f}s")
+
+print("Done!")
