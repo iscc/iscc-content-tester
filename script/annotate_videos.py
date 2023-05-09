d26b40a36301f8267377e64991b82576109084ca
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-09 13:16:48 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -10,35 +10,36 @@ src_dir = '/iscc/videos720/'
 dest_dir = '/iscc/annotated_videos/'
 
 total_videos = sum(len(data[key][sub_key]) for key in data for sub_key in data[key])
-pbar = tqdm(total=total_videos, desc="Copying videos", unit=" video")
+pbar = tqdm(total=total_videos, desc="Copying videos")
 
 for key in data:
     print(f"Processing key: {key}")
-    if key + '.mp4' not in os.listdir(src_dir):
-        print(f"{key}.mp4 not found in {src_dir}. Skipping...")
+    if not os.path.exists(os.path.join(src_dir, f"{key}.mp4")):
+        print(f"Video for key {key} not found in {src_dir}")
         continue
     
     dest_key_dir = os.path.join(dest_dir, key)
     os.makedirs(dest_key_dir, exist_ok=True)
     
     for sub_key in data[key]:
-        print(f"Processing subkey: {sub_key}")
+        print(f"Processing sub_key: {sub_key}")
         dest_sub_dir = os.path.join(dest_key_dir, sub_key)
         os.makedirs(dest_sub_dir, exist_ok=True)
         
         for video_id in data[key][sub_key]:
-            src_path = os.path.join(src_dir, key + '.mp4')
-            dest_path = os.path.join(dest_sub_dir, video_id + '.mp4')
+            print(f"Processing video: {video_id}")
+            src_path = os.path.join(src_dir, f"{key}.mp4")
+            dest_path = os.path.join(dest_sub_dir, f"{video_id}.mp4")
             
             if not os.path.isfile(src_path):
-                print(f"{src_path} not found. Skipping...")
+                print(f"Source file {src_path} not found")
                 pbar.update(1)
                 continue
             
-            print(f"Copying {src_path} to {dest_path}")
             shutil.copyfile(src_path, dest_path)
             pbar.update(1)
             remaining_videos = total_videos - pbar.n
             pbar.set_postfix(completed=f"{pbar.n}/{total_videos}", eta=f"{remaining_videos // pbar.rate:.0f}s")
-
-print("Done!")
+            print(f"Copied video {video_id} to {dest_path}")
+            
+print("Copying complete!")
