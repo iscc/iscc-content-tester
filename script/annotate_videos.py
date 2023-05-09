b59e9f9069adec85562f79a2091e3a6e9c2bd96b
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-09 14:03:00 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -10,24 +10,15 @@ src_dir = '/iscc/videos720/'
 dest_dir = '/iscc/annotated_videos/'
 
 total_videos = sum(len(data[key][sub_key]) for key in data for sub_key in data[key])
-pbar = tqdm(total=total_videos + len(data), desc="Copying videos")
+pbar = tqdm(total=total_videos, desc="Copying videos")
 
 for key in data:
     if key + ".mp4" not in os.listdir(src_dir):
         print(f"{key}.mp4 not found in {src_dir}")
-        pbar.update(1)
         continue
     
-    dest_key_dir = os.path.join(dest_dir, key)
-    os.makedirs(dest_key_dir, exist_ok=True)
-    
-    src_path = os.path.join(src_dir, key + ".mp4")
-    dest_path = os.path.join(dest_key_dir, key + ".mp4")
-    shutil.copyfile(src_path, dest_path)
-    pbar.update(1)
-    
     for sub_key in data[key]:
-        dest_sub_dir = os.path.join(dest_key_dir, sub_key)
+        dest_sub_dir = os.path.join(dest_dir, key, sub_key)
         os.makedirs(dest_sub_dir, exist_ok=True)
         
         for video_id in data[key][sub_key]:
@@ -40,8 +31,10 @@ for key in data:
                 continue
             
             shutil.copyfile(src_path, dest_path)
+            if not os.path.isfile(os.path.join(dest_sub_dir, key + '.mp4')):
+                shutil.copyfile(src_path, os.path.join(dest_sub_dir, key + '.mp4'))
             pbar.update(1)
-            completed_videos = pbar.n - 1
+            completed_videos = pbar.n
             remaining_videos = total_videos - completed_videos
             pbar.set_postfix(completed=f"{completed_videos}/{total_videos}", remaining=f"{remaining_videos} videos")
             
