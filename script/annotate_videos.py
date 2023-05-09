8fa2574c2292b43caf7c90fab7554809f596204b
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-09 13:05:22 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -9,28 +9,29 @@ with open('/iscc/git/FIVR-200K/dataset/annotation.json') as f:
 src_dir = '/iscc/videos720/'
 dest_dir = '/iscc/annotated_videos/'
 
-available_videos = []
-for key in data:
-    if key in os.listdir(src_dir):
-        available_videos.extend([key + '/' + video_id for sub_key in data[key] for video_id in data[key][sub_key]])
+# Get a list of available primary keys
+available_primary_keys = [key for key in data if os.path.isdir(os.path.join(src_dir, key))]
 
-total_videos = len(available_videos)
+total_videos = sum(len(data[key][sub_key]) for key in available_primary_keys for sub_key in data[key])
 pbar = tqdm(total=total_videos, desc="Copying videos")
 
-for video_id in available_videos:
-    key, video_id = video_id.split('/')
+for key in available_primary_keys:
+    dest_key_dir = os.path.join(dest_dir, key)
+    os.makedirs(dest_key_dir, exist_ok=True)
+    
     for sub_key in data[key]:
-        if video_id in data[key][sub_key]:
+        dest_sub_dir = os.path.join(dest_key_dir, sub_key)
+        os.makedirs(dest_sub_dir, exist_ok=True)
+        
+        for video_id in data[key][sub_key]:
             src_path = os.path.join(src_dir, key, video_id + '.mp4')
-            dest_key_dir = os.path.join(dest_dir, key)
-            dest_sub_dir = os.path.join(dest_key_dir, sub_key)
             dest_path = os.path.join(dest_sub_dir, video_id + '.mp4')
             
-            os.makedirs(dest_sub_dir, exist_ok=True)
+            if not os.path.isfile(src_path):
+                pbar.update(1)
+                continue
+            
             shutil.copyfile(src_path, dest_path)
             pbar.update(1)
             remaining_videos = total_videos - pbar.n
             pbar.set_postfix(eta=f"{remaining_videos // pbar.avg:.0f}s")
-            break
-else:
-    pbar.close()
