0cb712bd1f8baf195a3277334f85dc3b4ae95608
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-09 12:49:14 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -7,32 +7,36 @@ annotation_file = "/iscc/git/FIVR-200K/dataset/annotation.json"
 src_dir = "/iscc/videos720/"
 dest_dir = "/iscc/annotated_videos/"
 
-with open(annotation_file) as f:
+with open(annotation_file, "r") as f:
     data = json.load(f)
 
-total_videos = sum(len(v) for k in data for v in data[k].values())
+for key, values in tqdm(data.items()):
+    for sub_key, sub_values in values.items():
+        sub_folder = os.path.join(dest_dir, sub_key)
+        os.makedirs(sub_folder, exist_ok=True)
+        for video_id in sub_values:
+            src_path = os.path.join(src_dir, f"{video_id}.mp4")
+            dest_path = os.path.join(sub_folder, f"{video_id}.mp4")
+            if not os.path.isfile(src_path):
+                tqdm.write(f"{src_path} does not exist. Skipping...")
+                continue
+            shutil.copyfile(src_path, dest_path)
+    os.rename(os.path.join(dest_dir, key + ".mp4"), os.path.join(dest_dir, sub_folder, key + ".mp4"))
 
-with tqdm(total=total_videos, unit="videos", dynamic_ncols=True) as pbar:
-    for video_id, video_data in data.items():
-        video_path = os.path.join(src_dir, video_id + ".mp4")
-        if not os.path.exists(video_path):
-            pbar.write(f"Skipping {video_id}: file not found")
-            continue
-
-        for query, query_videos in video_data.items():
-            query_path = os.path.join(dest_dir, query)
-            os.makedirs(query_path, exist_ok=True)
-
-            for query_video in query_videos:
-                src_path = os.path.join(src_dir, query_video + ".mp4")
-                dest_path = os.path.join(query_path, query_video + ".mp4")
-
-                if os.path.exists(dest_path):
-                    pbar.write(f"Skipping {dest_path}: file already exists")
-                    pbar.update(1)
+remaining_videos = sum(len(v) for v in data.values())
+with tqdm(total=remaining_videos, desc="Copying files") as pbar:
+    for key, values in data.items():
+        for sub_key, sub_values in values.items():
+            for video_id in sub_values:
+                src_path = os.path.join(src_dir, f"{video_id}.mp4")
+                dest_path = os.path.join(dest_dir, sub_key, f"{video_id}.mp4")
+                if not os.path.isfile(src_path):
+                    pbar.write(f"{src_path} does not exist. Skipping...")
+                    remaining_videos -= 1
+                    pbar.total = remaining_videos
                     continue
-
                 shutil.copyfile(src_path, dest_path)
                 pbar.update(1)
-                remaining_videos = total_videos - pbar.n
-                pbar.set_postfix(eta=f"{remaining_videos // pbar.avg:.0f}s")
+                remaining_videos -= 1
+                pbar.total = remaining_videos
+                pbar.set_postfix(eta=f"{remaining_videos // pbar.n * pbar.refresh_rate:.0f}s")
