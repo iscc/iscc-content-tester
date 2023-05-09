f105a803f611733327ef1e90bd7e67e17dcd5678
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-09 13:06:26 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -9,13 +9,14 @@ with open('/iscc/git/FIVR-200K/dataset/annotation.json') as f:
 src_dir = '/iscc/videos720/'
 dest_dir = '/iscc/annotated_videos/'
 
-# Get a list of available primary keys
-available_primary_keys = [key for key in data if os.path.isdir(os.path.join(src_dir, key))]
-
-total_videos = sum(len(data[key][sub_key]) for key in available_primary_keys for sub_key in data[key])
+total_videos = sum(len(data[key][sub_key]) for key in data for sub_key in data[key])
 pbar = tqdm(total=total_videos, desc="Copying videos")
 
-for key in available_primary_keys:
+for key in data:
+    if key not in os.listdir(src_dir):
+        print(f"Skipping {key}: source directory does not exist")
+        continue
+    
     dest_key_dir = os.path.join(dest_dir, key)
     os.makedirs(dest_key_dir, exist_ok=True)
     
@@ -28,10 +29,12 @@ for key in available_primary_keys:
             dest_path = os.path.join(dest_sub_dir, video_id + '.mp4')
             
             if not os.path.isfile(src_path):
+                print(f"Skipping {src_path}: file does not exist")
                 pbar.update(1)
                 continue
             
             shutil.copyfile(src_path, dest_path)
             pbar.update(1)
+            print(f"Copied {src_path} to {dest_path}")
             remaining_videos = total_videos - pbar.n
             pbar.set_postfix(eta=f"{remaining_videos // pbar.avg:.0f}s")
