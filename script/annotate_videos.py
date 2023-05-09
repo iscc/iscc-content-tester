a661e85a08254aa31c5edb98038948a1bec165c2
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-09 13:21:08 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -13,34 +13,31 @@ total_videos = sum(len(data[key][sub_key]) for key in data for sub_key in data[k
 pbar = tqdm(total=total_videos, desc="Copying videos")
 
 for key in data:
-    print(f"Processing key: {key}")
-    if not os.path.exists(os.path.join(src_dir, f"{key}.mp4")):
-        print(f"Video for key {key} not found in {src_dir}")
+    if key + ".mp4" not in os.listdir(src_dir):
+        print(f"{key}.mp4 not found in {src_dir}")
         continue
     
     dest_key_dir = os.path.join(dest_dir, key)
     os.makedirs(dest_key_dir, exist_ok=True)
     
     for sub_key in data[key]:
-        print(f"Processing sub_key: {sub_key}")
         dest_sub_dir = os.path.join(dest_key_dir, sub_key)
         os.makedirs(dest_sub_dir, exist_ok=True)
         
         for video_id in data[key][sub_key]:
-            print(f"Processing video: {video_id}")
-            src_path = os.path.join(src_dir, f"{key}.mp4")
-            dest_path = os.path.join(dest_sub_dir, f"{video_id}.mp4")
+            src_path = os.path.join(src_dir, key + ".mp4")
+            dest_path = os.path.join(dest_sub_dir, video_id + '.mp4')
             
             if not os.path.isfile(src_path):
-                print(f"Source file {src_path} not found")
+                print(f"{src_path} not found")
                 pbar.update(1)
                 continue
             
             shutil.copyfile(src_path, dest_path)
             pbar.update(1)
-            remaining_videos = total_videos - pbar.n
-            pbar.set_postfix(completed=f"{pbar.n}/{total_videos}", 
-                             eta=f"{remaining_videos // pbar.refresh_rate:.0f}s")
-            print(f"Copied video {video_id} to {dest_path}")
+            completed_videos = pbar.n
+            remaining_videos = total_videos - completed_videos
+            pbar.set_postfix(completed=f"{completed_videos}/{total_videos}", remaining=f"{remaining_videos} videos")
             
+pbar.close()
 print("Copying complete!")
