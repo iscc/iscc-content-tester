14bf0dac1a50e55583f5755528aef7322c6e9cfe
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-09 13:07:31 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -13,8 +13,8 @@ total_videos = sum(len(data[key][sub_key]) for key in data for sub_key in data[k
 pbar = tqdm(total=total_videos, desc="Copying videos")
 
 for key in data:
-    if key not in os.listdir(src_dir):
-        print(f"Skipping {key}: source directory does not exist")
+    if not os.path.exists(os.path.join(src_dir, key+'.mp4')):
+        print(f"Skipping primary key {key}. Video file not found.")
         continue
     
     dest_key_dir = os.path.join(dest_dir, key)
@@ -29,12 +29,13 @@ for key in data:
             dest_path = os.path.join(dest_sub_dir, video_id + '.mp4')
             
             if not os.path.isfile(src_path):
-                print(f"Skipping {src_path}: file does not exist")
+                print(f"Skipping video {src_path}. File not found.")
                 pbar.update(1)
                 continue
             
             shutil.copyfile(src_path, dest_path)
             pbar.update(1)
-            print(f"Copied {src_path} to {dest_path}")
             remaining_videos = total_videos - pbar.n
             pbar.set_postfix(eta=f"{remaining_videos // pbar.avg:.0f}s")
+
+print("Finished copying videos.")
