f47bfb726f7eb6c9d06b852b1c9adf34fae8760e
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-09 12:47:01 +0200

    Update annotate_videos.py

--- script/annotate_videos.py
+++ script/annotate_videos.py
@@ -1,32 +1,38 @@
 import os
-import shutil
 import json
+import shutil
 from tqdm import tqdm
 
-# set input and output directories
-input_dir = "/iscc/videos720/"
-output_dir = "/iscc/annotated_videos/"
+annotation_file = "/iscc/git/FIVR-200K/dataset/annotation.json"
+src_dir = "/iscc/videos720/"
+dest_dir = "/iscc/annotated_videos/"
 
-# load annotation data
-with open('/iscc/git/FIVR-200K/dataset/annotation.json', 'r') as f:
-    annotation = json.load(f)
+with open(annotation_file) as f:
+    data = json.load(f)
 
-# iterate over each video and copy it to the corresponding directory
-total_videos = sum(len(v) for k, v in annotation.items() for kk, vv in v.items() for _ in vv)
-with tqdm(total=total_videos, desc="Copying videos") as pbar:
-    for video_id, video_data in annotation.items():
-        video_dir = os.path.join(output_dir, video_id)
-        os.makedirs(video_dir, exist_ok=True)
-        for query_type, query_list in video_data.items():
-            query_dir = os.path.join(video_dir, query_type)
-            os.makedirs(query_dir, exist_ok=True)
-            for query in query_list:
-                src_path = os.path.join(input_dir, query + ".mp4")
-                dest_path = os.path.join(query_dir, query + ".mp4")
-                if os.path.exists(src_path):
-                    shutil.copyfile(src_path, dest_path)
+total_videos = sum(len(v) for k in data for v in data[k].values())
+
+with tqdm(total=total_videos, unit="videos", dynamic_ncols=True) as pbar:
+    for video_id, video_data in data.items():
+        video_path = os.path.join(src_dir, video_id + ".mp4")
+        if not os.path.exists(video_path):
+            pbar.write(f"Skipping {video_id}: file not found")
+            continue
+
+        for query, query_videos in video_data.items():
+            query_path = os.path.join(dest_dir, query)
+            os.makedirs(query_path, exist_ok=True)
+
+            for query_video in query_videos:
+                src_path = os.path.join(src_dir, query_video + ".mp4")
+                dest_path = os.path.join(query_path, query_video + ".mp4")
+
+                if os.path.exists(dest_path):
+                    pbar.write(f"Skipping {dest_path}: file already exists")
                     pbar.update(1)
-                    remaining_videos = total_videos - pbar.n
-                    pbar.set_postfix(eta=f"{remaining_videos // pbar.rate:.0f}s")
-                else:
-                    print(f"Skipping {src_path}: File does not exist")
+                    continue
+
+                shutil.copyfile(src_path, dest_path)
+                pbar.update(1)
+                remaining_videos = total_videos - pbar.n
+                pbar.set_postfix(eta=f"{remaining_videos // pbar.avg:.0f}s")
