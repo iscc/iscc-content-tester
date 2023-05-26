230f45cd68ad4229e97e2e13e79e5f1f39425ab6
Kevin Wollowski <64931775+wollooo@users.noreply.github.com>
2023-05-26 10:49:56 +0200

    Create pdf_text_processing_exp1.py

---
+++ script/pdf_text_processing_exp1.py
@@ -0,0 +1,79 @@
+import os
+import shutil
+from functools import partial
+from multiprocessing import Pool, cpu_count
+from tqdm import tqdm
+from pathlib import Path
+from tika import parser
+import warnings
+
+# Configuration
+PDF_DIR = "/iscc/openalex-pdfs"
+OUTPUT_DIR = "/iscc/pdfs"
+CUTOFF_PERCENTAGE = 10  # Remove 10% of text from the beginning of the half text
+PROCESS_PDF_COUNT = 1000  # Number of PDFs to process
+PROCESSES = 16  # Number of processes
+
+def extract_and_modify_pdf(output_dir, cutoff_percentage, input_pdf):
+    pdf_folder_name = Path(input_pdf).stem
+    pdf_output_dir = os.path.join(output_dir, pdf_folder_name)
+
+    try:
+        # Parse the PDF using Tika
+        parsed_pdf = parser.from_file(input_pdf)
+        original_text = parsed_pdf['content']
+
+        # Calculate the number of characters to remove (10% of the beginning of the half text)
+        text_length = len(original_text)
+        remove_chars = int(text_length * cutoff_percentage / 100 // 2)
+
+        # Split the text into the original and the modified parts
+        modified_text = original_text[remove_chars:]
+
+        os.makedirs(pdf_output_dir, exist_ok=True)
+
+        output_txt = os.path.join(pdf_output_dir, f"{pdf_folder_name}_collapsed.txt")
+        original_txt = os.path.join(pdf_output_dir, f"{pdf_folder_name}.txt")
+
+        # Save the original PDF as plain text
+        with open(original_txt, 'w', encoding='utf-8') as original_file:
+            original_file.write(original_text)
+
+        # Save the modified PDF as plain text
+        with open(output_txt, 'w', encoding='utf-8') as output_file:
+            output_file.write(modified_text)
+
+        # Paste the cutout in the next 1000 texts
+        for i in range(1, 1001):
+            next_txt_path = os.path.join(output_dir, f"text_{i}.txt")
+            with open(next_txt_path, 'a', encoding='utf-8') as next_txt_file:
+                next_txt_file.write(original_text[:remove_chars])
+
+    except Exception as e:
+        print(f"Error processing {input_pdf}: {e}")
+        return input_pdf
+
+    return input_pdf
+
+def process_pdfs(pdf_list, output_dir, cutoff_percentage):
+    with Pool(PROCESSES) as pool:
+        func = partial(extract_and_modify_pdf, output_dir, cutoff_percentage)
+        progress_bar = tqdm(total=len(pdf_list), desc="Processing PDFs")
+        for processed_file in pool.imap_unordered(func, pdf_list):
+            progress_bar.update(1)
+            progress_bar.set_postfix({"Processed": Path(processed_file).name})
+    progress_bar.close()
+
+if __name__ == "__main__":
+    pdf_files = [os.path.join(PDF_DIR, f) for f in os.listdir(PDF_DIR) if f.endswith(".pdf")]
+    pdf_files = pdf_files[:PROCESS_PDF_COUNT]
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+    print("Parameters:")
+    print(f"PDF Directory: {PDF_DIR}")
+    print(f"Output Directory: {OUTPUT_DIR}")
+    print(f"Cutoff Percentage: {CUTOFF_PERCENTAGE}%")
+    print(f"Number of PDFs to process: {PROCESS_PDF_COUNT}")
+    print(f"Number of processes: {PROCESSES}\n")
+
+    process_pdfs(pdf_files, OUTPUT_DIR, CUTOFF_PERCENTAGE)
